a5f88fa4a305de63553ca4255c7920b1db1e4ab5
newt0 <py_lambda@yahoo.co.jp>
2021-02-13 13:49:43 +0900

    add: Storeとの接続  Stateを管理する

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -1,4 +1,20 @@
 import { createStore as reduxCreateStore, combineReducers } from "redux";
-import {} from "../products/reducers";
-// 1. reduxモジュールのimport
-// 2. Reducersのimport
+import { UsersReducer } from "../users/reducers";
+
+export default function createStore() {
+  return reduxCreateStore(combineReducers({ users: UsersReducer }));
+}
+
+// ## 1. reduxモジュールのimport
+// ## 2. Reducersのimport
+
+// ## createStore関数の定義
+// 1. reduxのcreateStoreメソッドをreturn(reduxCreateStoreという別名をつけた)
+// // 引数にcombineReducersを受け取る
+// 2. combineReducersでstateを生成
+// // usersというキーに対してUsersReducerを渡す
+
+// ## combineReducersとは
+// 1.分割したReducersをまとめる
+// 2.stateのカテゴリ別
+// 3.オブジェクトをreturnする（stateのデータ構造）
