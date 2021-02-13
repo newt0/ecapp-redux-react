902a2925e6360e73bfeeed38f55bb9db3edd7154
newt0 <py_lambda@yahoo.co.jp>
2021-02-13 13:02:33 +0900

    fix: ディレクトリの配置ミス（users, products, storeを並列にするつもりがusersの中に入れてしまっていた）

---
+++ src/reducks/store/store.js
@@ -0,0 +1,4 @@
+import { createStore as reduxCreateStore, combineReducers } from "redux";
+import {} from "../products/reducers";
+// 1. reduxモジュールのimport
+// 2. Reducersのimport
---
+++ src/reducks/users/store/store.js
@@ -0,0 +1,5 @@
+import { createStore as reduxCreateStore, combineReducers } from "redux";
+import {} from "../products/reducers";
+import {} from "../";
+// 1. reduxモジュールのimport
+// 2. Reducersのimport
