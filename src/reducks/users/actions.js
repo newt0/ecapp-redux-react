c162fa710001724b276025c0e216ea4ab80ce58d
newt0 <py_lambda@yahoo.co.jp>
2021-02-13 09:45:13 +0900

    add: Actionsとは何か

---
+++ src/reducks/users/actions.js
@@ -0,0 +1,32 @@
+export const SIGN_IN = "SIGN_IN";
+export const signInAction = (userState) => {
+  return {
+    type: "SIGN_IN",
+    payload: {
+      isSignedIn: true,
+      uid: userState.uid,
+      username: userState.username,
+    },
+  };
+};
+
+export const SIGN_OUT = "SIGN_OUT";
+export const signOutAction = (userState) => {
+  return {
+    type: "SIGN_OUT",
+    payload: {
+      isSignedIn: false,
+      uid: "",
+      username: "",
+    },
+  };
+};
+
+// ## Actionsの役割
+// =アプリからStoreへデータを送るためのpayloadを渡す役割
+// ->アプリから受け取ったデータをReducersへ渡す
+
+// ## なぜActionsを使うのか？
+// ->純粋なデータのみ記述するため(どのStateをどう変更するかReducersへ任せる)
+
+// ## Actionsはプレーンなオブジェクトを返す
