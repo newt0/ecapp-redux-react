0be270928bc8e2ac9c63c8a25bdcde9818b981bb
newt0 <py_lambda@yahoo.co.jp>
2021-02-14 13:45:50 +0900

    fix: 不要なディレクトリ

--- src/reducks/users/store/store.js
+++
@@ -1,5 +0,0 @@
-import { createStore as reduxCreateStore, combineReducers } from "redux";
-import {} from "../products/reducers";
-import {} from "../";
-// 1. reduxモジュールのimport
-// 2. Reducersのimport
