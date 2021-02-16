014a2423aa201672188fbfe06da47222c1bacc4c
newt0 <py_lambda@yahoo.co.jp>
2021-02-16 15:10:15 +0900

    add: signinスクリーン作成

---
+++ src/templates/SignIn.jsx
@@ -0,0 +1,62 @@
+import React, { useCallback, useState } from "react";
+import { useDispatch } from "react-redux";
+import { PrimaryButton, TextInput } from "../components/UIkit";
+import { signIn } from "../reducks/users/operations";
+
+const SignIn = (props) => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState(""),
+    [password, setPassword] = useState("");
+
+  const inputEmail = useCallback(
+    (event) => {
+      setEmail(event.target.value);
+    },
+    [setEmail]
+  );
+
+  const inputPassword = useCallback(
+    (event) => {
+      setPassword(event.target.value);
+    },
+    [setPassword]
+  );
+
+  return (
+    <div className="c-section-container">
+      <h2 className="u-text-center u-text__headline">サインイン</h2>
+      <div className="module-spacer--medium" />
+      <TextInput
+        fullWidth={true}
+        label={"メールアドレス"}
+        margin="dense"
+        multiline={false}
+        required={true}
+        rows={1}
+        value={email}
+        type={"email"}
+        onChange={inputEmail}
+      />
+      <TextInput
+        fullWidth={true}
+        label={"パスワード"}
+        margin="dense"
+        multiline={false}
+        required={true}
+        rows={1}
+        value={password}
+        type={"password"}
+        onChange={inputPassword}
+      />
+      <div className="module-spacer--medium" />
+      <PrimaryButton
+        label={"サインインする"}
+        onChange={() => {
+          dispatch(signIn(email, password));
+        }}
+      />
+    </div>
+  );
+};
+
+export default SignIn;
