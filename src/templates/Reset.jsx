9148d266ebc0377cab0f3f25ca95ecac60f0fc7b
newt0 <py_lambda@yahoo.co.jp>
2021-02-16 16:57:19 +0900

    add: PWリセットスクリーンを追加

---
+++ src/templates/Reset.jsx
@@ -0,0 +1,40 @@
+import React, { useCallback, useState } from "react";
+import { useDispatch } from "react-redux";
+import { PrimaryButton, TextInput } from "../components/UIkit";
+import { resetPassword } from "../reducks/users/operations";
+
+const Reset = () => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const inputEmail = useCallback(
+    (event) => {
+      setEmail(event.target.value);
+    },
+    [setEmail]
+  );
+
+  return (
+    <div className="c-section-container">
+      <h2 className="u-text-center u-text__headline">パスワードのリセット</h2>
+      <div className="module-spacer--medium" />
+      <TextInput
+        fullWidth={true}
+        label={"メールアドレス"}
+        margin="dense"
+        multiline={false}
+        required={true}
+        rows={1}
+        value={email}
+        type={"email"}
+        onChange={inputEmail}
+      />
+      <div className="module-spacer--medium" />
+      <PrimaryButton
+        label={"パスワードをリセットする"}
+        onClick={() => dispatch(resetPassword(email))}
+      />
+    </div>
+  );
+};
+
+export default Reset;
