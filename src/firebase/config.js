608129eb4b528b5074285eab28c176c44e50889c
newt0 <py_lambda@yahoo.co.jp>
2021-02-16 13:19:57 +0900

    add: Firebase用の設定ファイルを作る

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCL0RkNQUkOIHbht6lwTom0_j6UgXX_h88",
+  authDomain: "ecapp-redux.firebaseapp.com",
+  databaseURL: "https://ecapp-redux.firebaseio.com",
+  projectId: "ecapp-redux",
+  storageBucket: "ecapp-redux.appspot.com",
+  messagingSenderId: "789398938636",
+  appId: "1:789398938636:web:a1a00cb421a25d56fc3a29",
+  measurementId: "G-1LELHJ3VE9",
+};
