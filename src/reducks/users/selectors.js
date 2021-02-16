3694f883dcbb5dc14cc4018270101a5d86c62e44
newt0 <py_lambda@yahoo.co.jp>
2021-02-16 15:54:50 +0900

    fix: selectors.jsにisSignedInを追加

--- src/reducks/users/selectors.js
+++ src/reducks/users/selectors.js
@@ -2,12 +2,18 @@ import { createSelector } from "reselect";
 
 const usersSelector = (state) => state.users;
 
+export const getIsSignedIn = createSelector(
+  [usersSelector],
+  (state) => state.isSignedIn
+);
+
 export const getUserId = createSelector([usersSelector], (state) => state.uid);
 
 export const getUsername = createSelector(
   [usersSelector],
   (state) => state.username
 );
+
 // ## 各ファイルの役割|seletor
 // 1.Store で制御している state を参照する役割
 // 2.reselect という npm モジュールを使う
