671d9b184d3a36e9cbc7f8ae4ae31bbc2cb54cac
newt0 <py_lambda@yahoo.co.jp>
2021-02-17 14:19:08 +0900

    fix: [WIP10] 空白が詰まっていた

--- src/components/Products/ImageArea.jsx
+++ src/components/Products/ImageArea.jsx
@@ -23,7 +23,7 @@ const ImageArea = (props) => {
       const S =
         "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
       const N = 16;
-      const fileName = Array.from(crypto.getRandomValues(newUint32Array(N)))
+      const fileName = Array.from(crypto.getRandomValues(new Uint32Array(N)))
         .map((n) => S[n % S.length])
         .join("");
 
