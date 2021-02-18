63c540ae015e562fad878ff35a3c6e6316dfce0b
newt0 <py_lambda@yahoo.co.jp>
2021-02-18 09:25:00 +0900

    fix: eslintを有効化してコードフォーマット。JSX内で直接props.imagesを使うやり方に戻す。useCallbackの第二引数をpropsに変更。prevStateの（）の付け方を変更。

--- src/components/Products/ImageArea.jsx
+++ src/components/Products/ImageArea.jsx
@@ -14,36 +14,42 @@ const useStyles = makeStyles({
 
 const ImageArea = (props) => {
   const classes = useStyles();
-  const images = props.images;
 
-  const uploadImage = useCallback((event) => {
-    const file = event.target.files;
-    let blob = new Blob(file, { type: "image/jpeg" });
+  const uploadImage = useCallback(
+    (event) => {
+      const file = event.target.files;
+      let blob = new Blob(file, { type: "image/jpeg" });
 
-    // Generate random 16 digits strings
-    const S="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    const N=16;
-    const fileName = Array.from(crypto.getRandomValues(new Uint32Array(N))).map((n)=>S[n%S.length]).join('')
+      // Generate random 16 digits strings
+      const S =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      const N = 16;
+      const fileName = Array.from(crypto.getRandomValues(new Uint32Array(N)))
+        .map((n) => S[n % S.length])
+        .join("");
 
-    const uploadRef = storage.ref('images').child(fileName);
-    const uploadTask = uploadRef.put(blob);
+      const uploadRef = storage.ref("images").child(fileName);
+      const uploadTask = uploadRef.put(blob);
 
-    uploadTask.then(() => {
-        uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-            const newImage = {id: fileName, path: downloadURL};
-            props.setImages((prevState => [...prevState, newImage]))
+      uploadTask
+        .then(() => {
+          uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+            const newImage = { id: fileName, path: downloadURL };
+            props.setImages((prevState) => [...prevState, newImage]);
+          });
+        })
+        .catch((error) => {
+          console.warn(error);
         });
-    }).catch((error) => {
-      console.warn(error)
-    });
-
-}, [props.setImages])
+    },
+    [props]
+  );
 
   return (
     <div>
       <div className="p-grid__list-images">
-        {images.length > 0 &&
-          images.map((image) => (
+        {props.images.length > 0 &&
+          props.images.map((image) => (
             <ImagePreview id={image.id} path={image.path} key={image.id} />
           ))}
       </div>
