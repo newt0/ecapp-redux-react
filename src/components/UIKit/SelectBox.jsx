18091bed87565495025afa401ce6ff155ce7682e
newt0 <py_lambda@yahoo.co.jp>
2021-02-18 10:10:51 +0900

    fix: SelectBoxを修正(You have provided an out-of-range value )

--- src/components/UIKit/SelectBox.jsx
+++ src/components/UIKit/SelectBox.jsx
@@ -1,40 +1,39 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
-import { makeStyles } from "@material-ui/styles";
 
-const useStyles = makeStyles({
-  formContorl: {
+const useStyles = makeStyles((theme) => ({
+  formControl: {
     marginBottom: 16,
-    minWidth: 128,
+    minWidth: 120,
     width: "100%",
   },
-});
+}));
 
 const SelectBox = (props) => {
   const classes = useStyles();
 
   return (
-    <FormControl className={classes.formContorl}>
+    <FormControl className={classes.formControl}>
       <InputLabel>{props.label}</InputLabel>
       <Select
+        value={props.value}
         required={props.required}
-        value={props.required}
-        onChange={(event) => props.select(event.target.value)}
+        onChange={(e) => props.select(e.target.value)}
       >
-        {props.options.map((option) => (
-          <MenuItem key={option.id} value={option.id}>
-            {option.name}
-          </MenuItem>
-        ))}
+        {props.options.map((value) => {
+          return (
+            <MenuItem key={value.id} value={value.id}>
+              {value.name}
+            </MenuItem>
+          );
+        })}
       </Select>
     </FormControl>
   );
 };
 
 export default SelectBox;
-
-// onChange={(event) => props.select(event.target.value)}
-// -> selectで選択された値が変わったら、その値を元にpropsで渡されきたselectという関数を実行する
