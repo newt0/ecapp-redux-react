5270cfac5e406f41d84c17bb59137feb4d70b0a9
newt0 <py_lambda@yahoo.co.jp>
2021-02-28 14:11:53 +0900

    fix: muiのimport文をまとめる

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import { push } from "connected-react-router";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 import NoImage from "../../assets/img/src/no_image.png";
 import { useDispatch } from "react-redux";
-import IconButton from "@material-ui/core/IconButton";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
 import { deleteProduct } from "../../reducks/products/operations";
 
 const useStyles = makeStyles((theme) => ({
