d3d7812ecdabf87551ad0bd5f03dc218b68f9c5f
newt0 <py_lambda@yahoo.co.jp>
2021-02-28 13:19:30 +0900

    fix: muiのimport文をまとめる

--- src/templates/CartList.jsx
+++ src/templates/CartList.jsx
@@ -1,7 +1,7 @@
 import React, { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductsInCart } from "../reducks/users/selectors";
-import List from "@material-ui/core/List";
+import { List } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { CartListItem } from "../components/Products";
 import { PrimaryButton, GreyButton } from "../components/UIkit";
